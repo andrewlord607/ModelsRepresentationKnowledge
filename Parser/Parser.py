04010f0ea0ba5a4436bbeb96c722ddd7cc11b9a8
andropov_a <andropov@geogracom.com>
2019-03-23 17:15:36 +0300

    Кое-что из этого теперь не актуально [+]Добавил несколько грамматических категорий [!]Попробовал сделать относительные пути [+]В класс сущностей добавил каноничную форму и квазиоснову. [+]В класс флексий добавил обработку разных частей речи(Не очень мне нравится) [!]Все split(" ") заменил на split(), чтобы избавится от пустых записей [+]Добавил словарь сложных лексем(эффективность так себе) [+]Добавил словарь предикатов, без обработки предикатов и почти один в один словарь сущностей. То есть надо их как-то обобщить. [!]Чуть-чуть поменял вывод

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -1,21 +1,23 @@
-from Parser.Dictionaries import ReadyPartDict, FlexiesDict, EntitiesDict, CharactersDict
+from Parser.Dictionaries import ReadyPartDict, FlexiesDict, EntitiesDict, GlueWordDict, PredicatesDict, CharactersDict
 
 
 class Parser(object):
     flexies = [FlexiesDict()]
-    dicts = [ReadyPartDict(), EntitiesDict(flexies), CharactersDict(flexies)]
+    dicts = [ReadyPartDict(), EntitiesDict(flexies), CharactersDict(flexies),  PredicatesDict(flexies)]
+    gw = GlueWordDict()
 
     def parse_input(self, input_str):
         input_str = input_str.strip().upper()
-        words = input_str.split(" ")
+        input_str = self.gw.transform(input_str)
+        words = input_str.split()
         phrases = []
         for word in words:
             phrases.append(self.parse_word(word))
-        return phrases
+        return '\n'.join(str(x) for x in phrases)
 
     def parse_word(self, word):
         for one_dict in self.dicts:
             parse = one_dict.find(word)
             if parse is not None:
-                return word + " " + parse.to_string()
+                return word + " - " + parse.to_string()
         return word + " Неопределенная часть речи"
--- bot.py
+++ bot.py
@@ -4,19 +4,20 @@ from telebot import apihelper
 from Parser.Parser import Parser
 
 apihelper.proxy = {'https': 'https://75.146.218.153:55768'}
+f = open('token.sec')
+bot = telebot.TeleBot(f.readline())
+f.close()
 
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): 
     print(str(message.chat.id)+": "+message.text)
-    bot.send_message(message.chat.id, parser.parse_input(message.text))
+    output_str = parser.parse_input(message.text)
+    print(output_str)
+    bot.send_message(message.chat.id, output_str)
 
 
 if __name__ == '__main__':
-    f = open('token.sec')
-    global bot
-    bot = telebot.TeleBot(f.readline())
-    f.close()
     global parser
     parser = Parser()
     bot.polling(none_stop=True, timeout=500000)
